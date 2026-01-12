7c9c2c32158785e3c286d58a287bec2fd87a315c
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-01-12 11:59:28 +0100

    Initial commit with models

---
+++ blog_platform_api/blog/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+#Blog post models with markdown support
+
+class Post(models.Model): 
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self): 
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+
